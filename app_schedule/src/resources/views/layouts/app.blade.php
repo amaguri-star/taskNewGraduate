978709649b490888d7826aa782b077c6a1f9b6f4
Ryusei <ryusei1597@gmail.com>
2022-01-22 17:16:53 +0900

    remove app.css code and reset

--- app_schedule/src/resources/views/layouts/app.blade.php
+++ app_schedule/src/resources/views/layouts/app.blade.php
@@ -15,8 +15,8 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
+    <link href="{{ asset('css/reset.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/calendar.css') }}" rel="stylesheet">
 
 </head>
 

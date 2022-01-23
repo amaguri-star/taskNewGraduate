297ef62e21131e516f53921269ae9be9e38d22c0
Ryusei <ryusei1597@gmail.com>
2022-01-23 14:37:03 +0900

    adjusted edit and createEvent page

--- app_schedule/src/resources/views/editEventPage.blade.php
+++ app_schedule/src/resources/views/editEventPage.blade.php
@@ -6,14 +6,17 @@
     <div class="container">
         <div class="card w-50 mx-auto">
             <div class="card-body">
-              <h5 class="card-title h5">{{ $date }}</h5>
-              <div class="form-outline my-3">
-                <input type="hidden" name="event_date" value="{{ $date }}">
-                <input type="text" name="title" id="eventForm" class="form-control"/>
-                <label class="form-label" for="eventForm">タスクを編集</label>
-              </div>
-              <button type="button" class="btn btn-primary">編集</button>
+                <h5 class="card-title h5">{{ $date }}</h5>
+                <form action="{{ route('events.update') }}" method="POST">
+                    @csrf
+                    <div class="form-outline my-3">
+                        <input type="hidden" name="event_date" value="{{ $date }}">
+                        <input type="text" name="title" id="eventForm" class="form-control" />
+                        <label class="form-label" for="eventForm">タスクを編集</label>
+                        <button type="button" class="btn btn-primary">編集</button>
+                    </div>
+                </form>
             </div>
-          </div>
+        </div>
     </div>
 @endsection

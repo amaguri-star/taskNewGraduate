05a05f85f8d89bdddd80b74403e4c5c53feed60f
Ryusei <ryusei1597@gmail.com>
2022-01-23 22:34:27 +0900

    add relation

--- app_schedule/src/app/Event.php
+++ app_schedule/src/app/Event.php
@@ -12,4 +12,9 @@ class Event extends Model
     protected $fillable = [
         'event_date', 'title'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }

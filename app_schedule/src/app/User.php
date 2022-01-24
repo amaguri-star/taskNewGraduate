7c98b29feb4b28e1df517ba8a9b4a34ff83b1e58
Ryusei <ryusei1597@gmail.com>
2022-01-24 14:16:29 +0900

    insert deleted_at into user table

--- app_schedule/src/app/User.php
+++ app_schedule/src/app/User.php
@@ -4,10 +4,12 @@ namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
